d456a7835c321d6ae084d94fcddfeb9cd15730b2
Holger Wansing <hwansing@mailbox.org>
2021-02-22 09:24:13 +0100

    Try to re-activate spellchecking for the installer

--- scripts/l10n/l10n-spellcheck/cfg/scheck.sh
+++ scripts/l10n/l10n-spellcheck/cfg/scheck.sh
@@ -14,19 +14,11 @@ l2)
 l3)
 	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/level3
 	;;
-l4)
-	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/level4
-	;;
-l5)
-	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/level5
-	;;
 all)
 	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/manual_d-i
 	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/level1
 	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/level2
 	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/level3
-	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/level4
-	l10n-spellcheck.sh ~/trunk/scripts/l10n/l10n-spellcheck/cfg/level5
 	;;
 *)
 	echo "Unknown parameter. Here's the list of known parameters:"
@@ -36,7 +28,5 @@ all)
 	echo "l1"
 	echo "l2"
 	echo "l3"
-	echo "l4"
-	echo "l5"
 	;;
 esac
