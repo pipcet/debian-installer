c5ad97e6f6764102da76a8fa24c6824cd58f42e8
Pip Cet <pipcet@gmail.com>
2021-12-25 22:21:30 +0000

    mondi.mk: clone the right branch

--- mondi/mondi.mk
+++ mondi/mondi.mk
@@ -44,7 +44,7 @@ $(BUILD)/debian/root1.cpio.gz: | $(BUILD)/debian/
 $(BUILD)/debian/script.bash: | $(BUILD)/debian/
 	(echo "#!/bin/bash -e"; \
 	echo "apt-get install ca-certificates"; \
-	echo "cd /root; git clone https://github.com/pipcet/debian-installer"; \
+	echo "cd /root; git clone https://github.com/pipcet/debian-installer -b mondi"; \
 	echo "cd /root/debian-installer/packages/anna; ./debian/rules build"; \
 	echo "cd /root/debian-installer/packages/anna; ./debian/rules binary"; \
 	echo "cp /root/debian-installer/packages/anna__*_arm64.udeb /root/debian-installer/installer/build/localudebs/"; \
