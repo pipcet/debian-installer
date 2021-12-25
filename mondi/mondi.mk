cf9be10be7dcb991dbcf1cd4560a53472a49b913
Pip Cet <pipcet@gmail.com>
2021-12-25 22:27:10 +0000

    mondi.mk: still trying to avoid that rebuild...

--- mondi/mondi.mk
+++ mondi/mondi.mk
@@ -43,6 +43,9 @@ $(BUILD)/debian/root1.cpio.gz: | $(BUILD)/debian/
 
 $(BUILD)/debian/script.bash: | $(BUILD)/debian/
 	(echo "#!/bin/bash -e"; \
+	echo "apt --fix-broken install;" \
+	echo "apt-get -y update;" \
+	ecoh "apt-get -y dist-upgrade;" \
 	echo "apt-get install ca-certificates"; \
 	echo "cd /root; git clone https://github.com/pipcet/debian-installer -b mondi"; \
 	echo "cd /root/debian-installer/packages/anna; ./debian/rules build"; \
