d7637017f973551613e2b010eef13bef473fb3bf
Pip Cet <pipcet@gmail.com>
2021-12-26 10:44:19 +0000

    mondi.mk: install the build-deps

--- mondi/mondi.mk
+++ mondi/mondi.mk
@@ -43,10 +43,14 @@ $(BUILD)/debian/root1.cpio.gz: | $(BUILD)/debian/
 
 $(BUILD)/debian/script.bash: | $(BUILD)/debian/
 	(echo "#!/bin/bash -x"; \
+	echo "echo deb-src https://deb.debian.org/debian sid main >> /etc/apt/sources.list"; \
 	echo "apt -y --fix-broken install"; \
 	echo "apt-get -y update"; \
 	echo "apt-get -y dist-upgrade"; \
 	echo "apt-get -y install ca-certificates || true"; \
+	echo "apt-get -y build-dep debian-installer anna busybox;" \
+	echo "apt-get install ca-certificates"; \
+	echo "apt-get clean"; \
 	echo "cd /root; git clone https://github.com/pipcet/debian-installer -b mondi"; \
 	echo "cd /root/debian-installer/packages/anna; ./debian/rules build"; \
 	echo "cd /root/debian-installer/packages/anna; ./debian/rules binary"; \
